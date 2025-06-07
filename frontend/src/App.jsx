c267b02c4a4c6153e4ba4bb4f2f6bd34334d6003
dharamvir <dharamvir78914@gmail.com>
2025-06-07 15:35:54 +0530

    Add footer section and app download component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer.jsx'
 const App = () => {
   return (<>
      <div className="app">
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/order" element={<PlaceOrder/>} />
       </Routes>
      </div>
+     <Footer />
      </>
   )
 }
