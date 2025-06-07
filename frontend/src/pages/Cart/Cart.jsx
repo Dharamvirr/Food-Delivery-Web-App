9a4a735e7e8ddb456da9c2b8f9679c1d162c2641
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-07 22:30:43 +0530

    basic structure of cart page added working on css

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import './Cart.css'
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
-export default Cart
+const Cart = () => {
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url } = useContext(StoreContext);
+
+  return (
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={item._id}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={url + "/images/" + item.image} alt={item.name} />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p className='cart-items-remove-icon' onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
