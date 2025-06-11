530a92c4124012bca2ee87163a5d1bbca1f4cada
dharamvir <dharamvir78914@gmail.com>
2025-06-11 12:00:49 +0530

    Add food management APIs: add, list, and remove food items

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 // index.js or server.js
 import dotenv from 'dotenv';
+import foodRouter from "./routes/foodRoute.js";
 dotenv.config();
 
 
@@ -16,12 +17,19 @@ const port = process.env.PORT ;
 app.use(express.json())
 app.use(cors()) // using this we can access any frontend from backend
 
+
+// db connection
+connectDB();
+
+// api endpoints 
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+
+
 app.get("/", (req, res) => {
     res.send("API Working")
   });
 
-// db connection
-connectDB();
 
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
