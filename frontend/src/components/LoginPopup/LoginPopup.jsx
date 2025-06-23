3ec38ed4c6a833ff180a2229c9264e0a22b0954e
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-23 13:18:21 +0530

    input handler for login signup added

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../Context/StoreContext'
@@ -8,17 +8,33 @@ import { StoreContext } from '../../Context/StoreContext'
 const LoginPopup = ({setShowLogin}) => {
     const [currState, setCurrState] = useState("Login");
 
-  return (
-    <div className='login-popup'>
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+    const onChangeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    useEffect(()=>{
+         console.log(data);
+    },[data]);
+
+    
+
+ return (
+        <div className='login-popup'>
             <form  className="login-popup-container">
                 <div className="login-popup-title">
-                    <h2>{currState}</h2> 
-                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+                    <h2>{currState}</h2> <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <div className="login-popup-inputs">
-                    {currState==="Login"?<></>:<input type="text" placeholder='Your name' required /> }
-                    <input name='email' type="email" placeholder='Your email' />
-                    <input name='password' type="password" placeholder='Password' required />
+                    {currState === "Sign Up" ? <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required /> : <></>}
+                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' />
+                    <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
                 </div>
                 <button>{currState === "Login" ? "Login" : "Create account"}</button>
                 <div className="login-popup-condition">
@@ -31,7 +47,7 @@ const LoginPopup = ({setShowLogin}) => {
                 }
             </form>
         </div>
-  )
+    )
 }
 
 export default LoginPopup
