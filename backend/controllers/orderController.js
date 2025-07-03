4dc682fa877c16659c732c039f2cd1e680000f26
dharamvir <dharamvir78914@gmail.com>
2025-07-03 18:43:16 +0530

    add verifyOrder function to handle post-payment validation

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -61,5 +61,21 @@ const placeOrder = async (req, res) => {
       
 
 }
+const verifyOrder = async (req, res) => {
+    const { orderId, success } = req.body;
+    try {
+        if (success === "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" })
+        }
+        else {
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({ success: false, message: "Not Paid" })
+        }
+    } catch (error) {
+        res.json({ success: false, message: "Not  Verified" })
+    }
 
-export {placeOrder}
+}
+
+export {placeOrder,verifyOrder}
