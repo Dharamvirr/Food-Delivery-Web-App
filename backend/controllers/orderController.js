489c36ad38fc91a06e8f3342ae140bb9eec8532e
dharamvir <dharamvir78914@gmail.com>
2025-07-06 17:50:05 +0530

    create API to list all orders for Admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -88,5 +88,15 @@ const verifyOrder = async (req, res) => {
     }
 
 }
+// Listing Order for Admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
 
-export {placeOrder,verifyOrder,userOrders}
+export {placeOrder,verifyOrder,userOrders,listOrders}
