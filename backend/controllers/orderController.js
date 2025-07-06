366b4b778b9303303c9e40cc3f790f623d0a3272
dharamvir <dharamvir78914@gmail.com>
2025-07-06 18:15:09 +0530

    add endpoint to update order status in database

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -99,4 +99,16 @@ const listOrders = async (req, res) => {
     }
 }
 
-export {placeOrder,verifyOrder,userOrders,listOrders}
+//api for updating order status
+const updateStatus = async (req, res) => {
+    console.log(req.body);
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" })
+    } catch (error) {
+        res.json({ success: false, message: "Error" })
+    }
+
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
