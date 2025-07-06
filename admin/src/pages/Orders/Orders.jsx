08e197ef72a856d47fe6d4eab66fd787f19c4b33
dharamvir <dharamvir78914@gmail.com>
2025-07-06 17:56:30 +0530

    fetch all orders in  Admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Orders.css'
-const Orders = () => {
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { assets, url, currency } from '../../assets/assets';
+
+const Order = () => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(`${url}/api/order/list`)
+    if (response.data.success) {
+      setOrders(response.data.data.reverse());
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+ 
+
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [])
+
   return (
-    <div>Orders</div>
+     <div>
+      
+     </div>
   )
 }
 
-export default Orders
+export default Order
